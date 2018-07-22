0fc54b8bcc4da63cb9a99e2e3be110af74fbaf0b
Dmytro Borysovskyi <bbbara10@gmail.com>
2018-07-22 15:17:22 +0300

    feat(03-css-variables): 03-CSSVariables

--- src/components/JavaScript30Days/03-CSSVariables/index.vue
+++ src/components/JavaScript30Days/03-CSSVariables/index.vue
@@ -6,9 +6,31 @@
 
   import mixin from '../mixin'
 
+  const BASE_VAR    = '--03-css-variables-base'
+  const SPACING_VAR = '--03-css-variables-spacing'
+  const BLUR_VAR    = '--03-css-variables-blur'
+
+  const cssVariablesComputedProperty = key => ({
+    get () {
+      return window.localStorage.getItem(key) || ''
+    },
+    set (value) {
+      return window.localStorage.setItem(key, value)
+    }
+  })
+
   export default {
     name: 'CSSVariables',
     mixins: [mixin(() => ({raw}), init)],
+    computed: {
+      base: cssVariablesComputedProperty(BASE_VAR),
+      spacing: cssVariablesComputedProperty(SPACING_VAR),
+      blur: cssVariablesComputedProperty(BLUR_VAR)
+    },
+    mounted () {
+      document.documentElement.style.setProperty(this.$refs.base.name, this.base)
+      this.$refs.base.value = this.base
+    }
   }
 </script>
 
--- src/components/JavaScript30Days/tabs.js
+++ src/components/JavaScript30Days/tabs.js
@@ -1,7 +1,7 @@
 export default [
   {title: '01-Drum Kit', component: () => import('./01-DrumKit'), active: true},
   {title: '02-JS and CSS Clock', component: () => import('./02-JSandCSSClock'), active: false},
-  // {title: '03-CSS Variables', component: () => import('./03-CSSVariables'), active: false},
+  {title: '03-CSS Variables', component: () => import('./03-CSSVariables'), active: false},
   // {title: '04-Array Cardio Day 1', component: () => import('./04-ArrayCardioDay-1'), active: false},
   // {title: '05-Flex Panel Gallery', component: () => import('./05-FlexPanelGallery'), active: false},
   // {title: '06-Type Ahead', component: () => import('./06-TypeAhead'), active: false},
