f26db9393689f72a1b90687910bd53b3b87f4589
Dmytro Borysovskyi <bbbara10@gmail.com>
2018-07-23 17:56:52 +0300

    feat(05-flex-panel-gallery): 05-FlexPanelGallery

--- src/components/JavaScript30Days/05-FlexPanelGallery/index.vue
+++ src/components/JavaScript30Days/05-FlexPanelGallery/index.vue
@@ -1,14 +1,19 @@
 <template src="./index.html"></template>
 
 <script>
-  import { init } from './index.vanilla'
-  import * as raw from '!raw-loader!./index.vanilla.js'
+  import { init }    from './index.vanilla'
+  import * as raw    from '!raw-loader!./index.vanilla.js'
+  import * as cssRaw from '!raw-loader!./style.css'
+
+  const ShowCode = () => import('@/components/ShowCode')
 
   import mixin from '../mixin'
 
   export default {
-    name: 'FlexPanelGallery',
-    mixins: [mixin(() => ({raw}), init)],
+    name:       'FlexPanelGallery',
+    components: {ShowCode},
+    mixins:     [mixin(() => ({raw}), init)],
+    data:       () => ({cssRaw})
   }
 </script>
 
