0613f84155dc738b6312641c0074b13a77fa6f41
Dmytro Borysovskyi <bbbara10@gmail.com>
2018-07-31 17:48:15 +0300

    feat(13-slide-in-on-scroll): 13-SlideInOnScroll

--- src/components/JavaScript30Days/13-SlideInOnScroll/index.vanilla.js
+++ src/components/JavaScript30Days/13-SlideInOnScroll/index.vanilla.js
@@ -1,3 +1,30 @@
 export function init () {
 
+  function debounce (fn, ms = 20) {
+    let timeoutId
+    return function (...args) {
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => fn.apply(this, args), ms)
+    }
+  }
+
+  const sliderImages = document.querySelectorAll('.slide-in')
+
+  function checkSlide () {
+    sliderImages.forEach(sliderImage => {
+      const slideInAt   = (window.scrollY + window.innerHeight) - sliderImage.clientHeight / 2
+      const imageBottom = sliderImage.offsetTop + sliderImage.clientHeight
+
+      const isHalfShown       = slideInAt > sliderImage.offsetTop
+      const isNotScrolledPast = window.scrollY < imageBottom
+
+      sliderImage.classList.toggle('active', isHalfShown && isNotScrolledPast)
+    })
+  }
+
+  window.addEventListener('scroll', debounce(checkSlide))
+
+  if (0 !== window.scrollY) {
+    window.scrollBy(0, window.scrollY - 1)
+  }
 }
