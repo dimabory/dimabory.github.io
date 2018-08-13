8d9f63f9a7dc503c6cc53ef5e28e2b87b251c810
Dmytro Borysovskyi <bbbara10@gmail.com>
2018-08-13 13:09:56 +0300

    feat(28-video-speed-controller): 28-VideoSpeedController

--- src/components/JavaScript30Days/28-VideoSpeedController/index.vanilla.js
+++ src/components/JavaScript30Days/28-VideoSpeedController/index.vanilla.js
@@ -1,3 +1,25 @@
 export function init () {
+  const speed = document.querySelector('.speed')
+  const bar   = speed.querySelector('.speed-bar')
+  const video = document.querySelector('.flex')
 
+  function updateSpeedBar (height, playbackRate) {
+    bar.style.height = height
+    bar.textContent  = playbackRate.toFixed(2) + 'x'
+  }
+
+  function updateVideoPlayback (playbackRate) {
+    video.playbackRate = playbackRate
+  }
+
+  speed.addEventListener('mousemove', function (e) {
+    const percent      = e.offsetY / this.offsetHeight
+    const min          = 0.4
+    const max          = 4
+    const height       = Math.round(percent * 100) + '%'
+    const playbackRate = percent * (max - min) + min
+
+    updateSpeedBar(height, playbackRate)
+    updateVideoPlayback(playbackRate)
+  })
 }
